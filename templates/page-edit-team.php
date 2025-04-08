896fe4ff6e8dc8f9b35f9a88271cabcdb2e05221
zhukowych <mzmzhuk@gmail.com>
2025-04-08 13:50:11 +0300

    update

--- templates/page-edit-team.php
+++ templates/page-edit-team.php
@@ -1,15 +1,8 @@
 <?php 
-
+echo "test";
 acf_form_head();
 
 get_header(); 
-
-$team_id = get_team_id();
-
-$team_captain_id = get_post_field('post_author', $team_id);
-
-$participants = get_field('participants', $team_id);
-echo $participants;
 
 render_page_by_id(12380);
 
